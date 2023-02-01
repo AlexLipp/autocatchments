b2f38a4baaf1ffd98640fb3bbf8fcf513dbaa82a
Alex Lipp <alex@lipp.org.uk>
2023-02-01 11:41:11 +0000

    Make function to load a grid from node arrays

--- autocatchments/toolkit.py
+++ autocatchments/toolkit.py
@@ -5,7 +5,9 @@ import numpy as np
 from landlab import RasterModelGrid
 from landlab.components import FlowAccumulator, SinkFillerBarnes
 from landlab.components.flow_accum.flow_accum_bw import (
-    find_drainage_area_and_discharge, make_ordered_node_array)
+    find_drainage_area_and_discharge,
+    make_ordered_node_array,
+)
 from matplotlib.colors import LogNorm
 from osgeo import gdal
 
@@ -30,8 +32,8 @@ def viz_drainage_area(grid: RasterModelGrid):
             grid.xy_of_lower_left[0] + grid.shape[1] * grid.dx,
             grid.xy_of_lower_left[1],
             grid.xy_of_lower_left[1] + grid.shape[0] * grid.dy,
-        ], 
-        cmap = 'Greys'
+        ],
+        cmap="Greys",
     )
     cb = matplotlib.pyplot.colorbar()
     cb.set_label("Drainage Area/units^2")
@@ -85,41 +87,6 @@ def read_geo_file(filename: str) -> Tuple[np.ndarray, gdal.Dataset]:
     band = ds.GetRasterBand(1)
     arr = band.ReadAsArray()
     return arr, ds
-
-
-def d8_to_receiver(d8: np.ndarray) -> np.ndarray:
-    """Converts a D8 raster array into an array of receiver nodes.
-    i.e. each node's value is the ID for the node to which it donates flow."""
-
-    rec = np.zeros(d8.shape) - 99
-    for i in np.arange(rec.shape[0]):  # rows
-        if i % 100 == 0:
-            print("Processing row", i, "of", rec.shape[0])
-        for j in np.arange(rec.shape[1]):  # cols
-            if i == 0 or i == (rec.shape[0] - 1) or j == 0 or j == (rec.shape[1] - 1):
-                # All boundary nodes are sinks
-                rec[i, j] = np.ravel_multi_index((i, j), d8.shape)
-            else:
-                if d8[i, j] == 0:  # Sink
-                    rec[i, j] = np.ravel_multi_index((i, j), d8.shape)
-                if d8[i, j] == 1:  # E
-                    rec[i, j] = np.ravel_multi_index((i, j + 1), d8.shape)
-                if d8[i, j] == 2:  # SE
-                    rec[i, j] = np.ravel_multi_index((i + 1, j + 1), d8.shape)
-                if d8[i, j] == 4:  # S
-                    rec[i, j] = np.ravel_multi_index((i + 1, j), d8.shape)
-                if d8[i, j] == 8:  # SW
-                    rec[i, j] = np.ravel_multi_index((i + 1, j - 1), d8.shape)
-                if d8[i, j] == 16:  # W
-                    rec[i, j] = np.ravel_multi_index((i, j - 1), d8.shape)
-                if d8[i, j] == 32:  # NW
-                    rec[i, j] = np.ravel_multi_index((i - 1, j - 1), d8.shape)
-                if d8[i, j] == 64:  # N
-                    rec[i, j] = np.ravel_multi_index((i - 1, j), d8.shape)
-                if d8[i, j] == 128:  # NE
-                    rec[i, j] = np.ravel_multi_index((i - 1, j + 1), d8.shape)
-    return rec.astype(int).flatten()
-
 
 def get_neighbour_indices(ind: int, array: np.ndarray) -> list[list]:
     """Gets indices of the nodes surrounding node `ind' (and itself) in array `array'.
@@ -175,9 +142,6 @@ def load_d8(path: str) -> RasterModelGrid:
     """Reads a (generic) geospatial file containing ESRI D8
     flow directions (e.g., 0, 1, ..., 128) and calculates
     drainage network, assigning this to a landlab RasterModelGrid.
-
-    TODO: Initiate the model nodes with a cell area
-
     """
 
     print("Reading infile")
@@ -191,6 +155,32 @@ def load_d8(path: str) -> RasterModelGrid:
     ordered_nodes = make_ordered_node_array(receivers)
     print("Initialising model grid")
     grid = RasterModelGrid(d8_arc.shape, xy_spacing=dx_dy, xy_of_lower_left=ll_xy)
+    _ = grid.add_field("flow__upstream_node_order", ordered_nodes)
+    _ = grid.add_field("flow__receiver_node", receivers)
+    print("Calculating drainage area")
+    a, _ = find_drainage_area_and_discharge(
+        ordered_nodes, receivers, node_cell_area=grid.dx * grid.dy
+    )
+    _ = grid.add_field("drainage_area", a)
+    return grid
+
+
+def load_from_node_arrays(
+    path_to_receiver_nodes: str,
+    path_to_ordered_nodes: str,
+    shape: Tuple[int, int],
+    xy_spacing: Tuple[float, float] = (1.0, 1.0),
+    xy_of_lower_left: Tuple[float, float] = (0, 0),
+) -> RasterModelGrid:
+    """Inititates a model grid from precalculated node arrays to speed up
+    computation"""
+
+    receivers = np.loadtxt(path_to_receiver_nodes).astype(np.int64)
+    ordered_nodes = np.loadtxt(path_to_ordered_nodes).astype(np.int64)
+    print("Initialising model grid")
+    grid = RasterModelGrid(
+        shape, xy_spacing=xy_spacing, xy_of_lower_left=xy_of_lower_left
+    )
     _ = grid.add_field("flow__upstream_node_order", ordered_nodes)
     _ = grid.add_field("flow__receiver_node", receivers)
     print("Calculating drainage area")
--- examples/load_rasters.py
+++ examples/load_rasters.py
@@ -5,6 +5,20 @@ mg = tk.load_d8("inputs/thames_d8.nc")
 tk.viz_drainage_area(mg)
 show()
 
+# Load from arrays of ordered nodes. 
+# Twice as fast but requires xy spacing
+# and lower left to be manually specified
+
+# mg = tk.load_from_node_arrays(
+#     path_to_receiver_nodes="thames_receiver_nodes.txt",
+#     path_to_ordered_nodes="thames_ordered_nodes.txt",
+#     shape=(1001,1001),
+#     xy_of_lower_left=(319975.0, 79975.0),
+#     xy_spacing= (50,50)
+# )
+# tk.viz_drainage_area(mg)
+# show()
+
 # Load a DEM
 mg = tk.load_topo("inputs/cairngorms_topo.asc")
 tk.viz_drainage_area(mg)
